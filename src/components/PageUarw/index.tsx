37b03692dc408697850d298288c04bd8ded0487a
Evgeny Barannikov <e.barannikov@7rlines.com>
2021-02-28 14:37:09 +0300

    uarw - select modes

--- src/components/PageUarw/index.tsx
+++ src/components/PageUarw/index.tsx
@@ -6,20 +6,27 @@ import './styles.scss'
 import { QCard } from './QCard';
 import { QCardOj, UarwLogic, UarwTuples } from '../../utils/uarw/uarw-logic';
 import { UARW_FE_PROGRESS, UARW_FE_SCOPES } from '../../consts-uarw';
+import { ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
 
 interface UarwState {
   uarwTuples: UarwTuples | null,
   loaded: boolean,
   loadedScopes: boolean,
+  scopesSelectMode: boolean,
   errStr: string,
   selectScOptions: object[],
   selectScSelectedOption: object | object[] | null,
   selectPrOptions: object[],
   selectPrSelectedOption: object | object[] | null,
   qcards: QCardOj[],
-  countAll: number
+  countAll: number,
+  selectMode: number
 }
 
+enum SelectMode {
+  STRICT = 1,
+  FREE = 2
+}
 
 function fnOptionsRefresh(currSelectOption: object | object[] | null, newVL: ValLabel[]): object | object[] | null {
   if (currSelectOption && newVL && newVL.length > 0) {
@@ -47,6 +54,7 @@ export class PageUarw extends Component<any, UarwState> {
     this.state = {
       loaded: false,
       loadedScopes: false,
+      scopesSelectMode: true,
       uarwTuples: null,
       errStr: '',
       selectScOptions: [],
@@ -54,11 +62,13 @@ export class PageUarw extends Component<any, UarwState> {
       selectPrOptions: [],
       selectPrSelectedOption: null,
       qcards: [],
-      countAll: 0
+      countAll: 0,
+      selectMode: SelectMode.STRICT
     }
     this.selectScHandleChange = this.selectScHandleChange.bind(this);
     this.selectPrHandleChange = this.selectPrHandleChange.bind(this);
     this.handleShowCards = this.handleShowCards.bind(this);
+    this.selectModeChange = this.selectModeChange.bind(this);
   }
 
   async componentDidMount() {
@@ -120,7 +130,9 @@ export class PageUarw extends Component<any, UarwState> {
 
   async selectHandleChange() {
     console.log(`!!-!!-!! -> :::::::::::::: selectHandleChange() {210227231432_1}:${Date.now()}`); // del+
-    await this.selectorsDataGetAndUpdate();
+    if (this.state.selectMode === SelectMode.STRICT) {
+      await this.selectorsDataGetAndUpdate();
+    }
   }
 
   async handleShowCards() {
@@ -141,6 +153,9 @@ export class PageUarw extends Component<any, UarwState> {
   }
 
   private fnFilterVuscGet() {
+    console.log('!!-!!-!! this.state.selectScSelectedOption {210228142743}\n', this.state.selectScSelectedOption); // del+
+    console.log('!!-!!-!! this.state.selectPrSelectedOption {210228142743}\n', this.state.selectPrSelectedOption); // del+
+
     let filterScVusc = selectOptionToVusc(UARW_FE_SCOPES, this.state.selectScSelectedOption as { value: string });
     let filterPrVusc = selectOptionToVusc(UARW_FE_PROGRESS, this.state.selectPrSelectedOption as { value: string });
     if (filterScVusc && filterPrVusc) {
@@ -150,6 +165,20 @@ export class PageUarw extends Component<any, UarwState> {
     } else {
       return filterPrVusc
     }
+  }
+
+  async selectModeChange(mode: SelectMode) {
+    console.log(`!!-!!-!! -> :::::::::::::: radioChange() {210228113307}:${Date.now()}`); // del+
+    console.log('!!-!!-!! mode {210228115358}\n', mode); // del+
+    this.setState({
+      selectMode: mode,
+      selectScSelectedOption: null,
+      selectPrSelectedOption: null
+    }, () => {
+      if(mode === SelectMode.FREE) {
+        this.selectorsDataGetAndUpdate()
+      }
+    })
   }
 
   render() {
@@ -183,6 +212,16 @@ export class PageUarw extends Component<any, UarwState> {
                 onChange={this.selectPrHandleChange}
                 isMulti
               />
+              <ToggleButtonGroup
+                className="select-mode"
+                name="value"
+                type="radio"
+                value={this.state.selectMode}
+                onChange={this.selectModeChange}
+              >
+                <ToggleButton value={1} variant="secondary" size="sm">Strict</ToggleButton>
+                <ToggleButton value={2} variant="secondary" size="sm">Free</ToggleButton>
+              </ToggleButtonGroup>
             </div>
             <div className="get-button">
               <input type="button" onClick={this.handleShowCards} value="get"/>
